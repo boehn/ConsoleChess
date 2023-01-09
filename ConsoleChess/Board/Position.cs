13512c078e604fc29866fc7711a07ec1c90aba31
Boen <ana.boen.jesus@gmail.com>
2023-01-08 22:19:06 -0300

    Added Position class

---
+++ ConsoleChess/Board/Position.cs
@@ -0,0 +1,21 @@
+using System.Text;
+
+namespace ConsoleChess.Board
+{
+    public class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return $"{Line}, {Column}";
+        }
+    }
+}
--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace ConsoleChess
+﻿using System;
+using ConsoleChess.Board;
+
+namespace ConsoleChess
 {
     internal class Program
     {
         public static void Main(string[] args)
-        { 
+        {
+            var position = new Position(3, 4);
+
+            Console.WriteLine($"Position: {position}");
+
         }
     }
 }
