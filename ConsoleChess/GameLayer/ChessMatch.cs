88a86a4b9759cbe761be3f4dc8ff06f2043501f6
Boen <ana.boen.jesus@gmail.com>
2023-01-14 00:33:55 -0300

    Implementing movement restriction

--- ConsoleChess/GameLayer/ChessMatch.cs
+++ ConsoleChess/GameLayer/ChessMatch.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using ConsoleChess.BoardLayer;
+using ConsoleChess.BoardLayer.CustomExceptions;
 
 namespace ConsoleChess.GameLayer
 {
     public class ChessMatch
     {
         public Board Board { get; private set; } = new Board(8, 8);
-        public int Shift { get; private set; } = 1;
+        public int Turn { get; private set; } = 1;
         public Color CurrentPlayer { get; private set; } = Color.White;
         public bool Finished { get; private set; } = false;
         private HashSet<Piece> _pieces = new HashSet<Piece>();
@@ -16,14 +17,46 @@ namespace ConsoleChess.GameLayer
             InsertPieces();
         }
         
-        public void ExecuteMove(Position origin, Position destiny)
+        private void ExecuteMove(Position origin, Position destiny)
         {
             var piece = Board.RemovePiece(origin);
             piece.IncrementMovesMade();
             var capturedPiece = Board.RemovePiece(destiny);
             Board.InsertPiece(piece, destiny);
         }
-        
+
+        public void PerformsMove(Position origin, Position destiny)
+        {
+            ExecuteMove(origin, destiny);
+            Turn++;
+            ChangePlayer();
+        }
+
+        public void ValidateOriginPosition(Position origin)
+        {
+            if (Board.Piece(origin) == null)
+            {
+                throw new BoardException("There is no piece in this position");
+            }
+
+            if (CurrentPlayer != Board.Piece(origin).Color)
+            {
+                throw new BoardException("This is not your piece");
+            }
+
+            if (!Board.Piece(origin).PossibleMovesExist())
+            {
+                throw new BoardException("There is not possible moves for you chosen piece");
+            }
+        }
+
+        public void ValidateDestinyPosition(Position origin, Position destiny)
+        {
+            if (!Board.Piece(origin).PossibleMove(destiny))
+            {
+                throw new BoardException("Destiny position is invalid");
+            }
+        }
         
         private void ChangePlayer()
         {
@@ -34,8 +67,8 @@ namespace ConsoleChess.GameLayer
         {
             return color == Color.White ? Color.Black : Color.White;
         }
-        
-        public void InsertNewPiece(char column, int line, Piece piece) {
+
+        private void InsertNewPiece(char column, int line, Piece piece) {
             Board.InsertPiece(piece, new ChessPosition(column, line).ToPosition());
             _pieces.Add(piece);
         }
--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -14,23 +14,36 @@ namespace ConsoleChess
                 var chessMatch = new ChessMatch();
                 while (!chessMatch.Finished)
                 {
-                    Console.Clear();
-                    Screen.PrintBoard(chessMatch.Board);
+                    try
+                    {
+                        Console.Clear();
+                        Screen.PrintBoard(chessMatch.Board);
+                        Console.WriteLine();
+                        Console.WriteLine($"Turn: {chessMatch.Turn}");
+                        Console.WriteLine($"Waiting player: {chessMatch.CurrentPlayer}");
                     
-                    Console.WriteLine();
-                    Console.Write("Origin: ");
-                    var origin = Screen.ReadPositionChess().ToPosition();
+                        Console.WriteLine();
+                        Console.Write("Origin: ");
+                        var origin = Screen.ReadPositionChess().ToPosition();
+                        chessMatch.ValidateOriginPosition(origin);
 
-                    var possibleMoves = chessMatch.Board.Piece(origin).PossibleMoves();
+                        var possibleMoves = chessMatch.Board.Piece(origin).PossibleMoves();
                     
-                    Console.Clear();
-                    Screen.PrintBoard(chessMatch.Board, possibleMoves);
+                        Console.Clear();
+                        Screen.PrintBoard(chessMatch.Board, possibleMoves);
                     
-                    Console.WriteLine();
-                    Console.Write("Destiny: ");
-                    var destiny = Screen.ReadPositionChess().ToPosition();
+                        Console.WriteLine();
+                        Console.Write("Destiny: ");
+                        var destiny = Screen.ReadPositionChess().ToPosition();
+                        chessMatch.ValidateDestinyPosition(origin, destiny);
                     
-                    chessMatch.ExecuteMove(origin, destiny);
+                        chessMatch.PerformsMove(origin, destiny);
+                    }
+                    catch (BoardException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
                 }
             }
             catch (BoardException e)
